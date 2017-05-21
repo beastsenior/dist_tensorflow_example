1cf2a05b9663fe6a0089c01b37130e4406da6274
beastsenior <beastsenior.qsh@gmail.com>
2017-05-21 07:41:08 -0400

    2017.5.21

--- ingraph.py
+++ ingraph.py
@@ -2,8 +2,7 @@ from datetime import datetime
 import tensorflow as tf
 import numpy as np
 
-
-server_target="grpc://205.172.170.17:12222"  #master
+server_target="grpc://172.16.100.2:12222"  #master
 # create data
 
 x_data = np.random.rand(100).astype(np.float32)
@@ -11,17 +10,17 @@ y_data = x_data*0.1 + 0.3
 
 ### create tensorflow structure start ###
 starttime=datetime.now()
-with tf.device("/job:work_server/task:2"):
+with tf.device("/job:canshu/task:0"):
     Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-# with tf.device("/job:canshu/task:1"):
+with tf.device("/job:canshu/task:1"):
     biases = tf.Variable(tf.zeros([1]))
 
-# with tf.device("/job:gongzuo/task:1"):
+with tf.device("/job:gongzuo/ps:0"):
     y = Weights*x_data + biases
     loss = tf.reduce_mean(tf.square(y-y_data))
 
-# with tf.device("/job:gongzuo/task:2"):
+with tf.device("/job:gongzuo/ps:1"):
     optimizer = tf.train.GradientDescentOptimizer(0.5)
     train = optimizer.minimize(loss)
     init = tf.global_variables_initializer()
---
+++ ingraph_claster_gongzuo0.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+tf.train.ClusterSpec({
+    "canshu": [
+        "172.16.100.2:12222",# /job:canshu/task:0 运行的主机
+        "172.16.100.3:12222",# /job:canshu/task:1 运行的主机
+    ],
+    "gongzuo": [
+        "172.16.100.4:12222",  # /job:gongzuo/task:0 运行的主机
+        "172.16.100.5:12222"   # /job:gongzuo/task:1 运行的主机
+    ]})
+
+server = tf.train.Server(cluster, job_name="gongzuo", task_index=0)
